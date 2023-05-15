240013baea22b38494668c0b7f70a2d02e718fce
L-Sindberg <esbensindberg@gmail.com>
2023-05-15 10:45:39 +0200

    Made changes to buttons on the recommendation screen and flex layout

--- src/screens/RecommendationModal.tsx
+++ src/screens/RecommendationModal.tsx
@@ -27,29 +27,52 @@ const RecommendationModal = (RecommendationModalProps: RecommendationModalProps)
     return (
         <SafeAreaView>
             <Modal visible={visible} animationType='slide'>
-                <View style={{ flex: 1 }}>
 
+                <View style={{
+                    flex: 8
+                }}>
                     <Image source={require('../assets/images/Top_WelcomeImage.png')} style={{
                         width: '100%',
-                        resizeMode: 'contain',
-                        marginTop: -45,
-
-
-
-
+                        height: '100%',
                     }
                     } />
+                </View>
+                <View style={{ flex: 1 }}>
                     <Text style={styles.title}>Interested?</Text>
-                    <Button title='close' onPress={onClose} />
-                    <View style={{ flex: 2 }}>
-                        <Pressable onPress={onClose} style={{
-                            justifyContent: 'flex-end'
-                        }}>
-                            <Text>close</Text>
-                        </Pressable>
-                    </View>
 
                 </View>
+                <View style={{ flex: 6, borderStyle: 'solid', ...styles.foodPicture }}>
+                    <Text>Picture</Text>
+                </View>
+                <View style={{ flex: 1 }}>
+                    <Text style={styles.nameText}>Name</Text>
+                </View>
+                <View style={{ flex: 1 }}>
+                    <Text>Info</Text>
+
+
+                </View>
+                <View style={{ flex: 3 }}>
+                    <View style={styles.bottomScreen}>
+                        <Pressable onPress={onClose} style={{
+                            backgroundColor: '#ba0909', ...styles.bottomButtons
+                        }} android_ripple={{ color: '#000', borderless: true, radius: 40, foreground: true }}>
+
+                            <Text style={styles.buttonText}>Nah</Text>
+
+                        </Pressable>
+
+                        <Pressable onPress={onClose} style={{
+                            backgroundColor: '#3fad10', ...styles.bottomButtons
+                        }} android_ripple={{ color: '#000', borderless: false, radius: 100, foreground: true }}>
+                            <Text style={styles.buttonText}>Yes!</Text>
+                        </Pressable>
+                    </View>
+                </View>
+
+
+
+
             </Modal>
         </SafeAreaView>
     );
@@ -58,10 +81,40 @@ const RecommendationModal = (RecommendationModalProps: RecommendationModalProps)
 const styles = StyleSheet.create({
     title: {
         color: '#1170ba',
-        fontSize: 42,
+        fontSize: 48,
         fontFamily: 'Satisfy-Regular',
         textAlign: 'center',
-        marginTop: -60
+        marginTop: '-5%'
+    },
+    foodPicture: {
+        borderColor: '#1170ba',
+        borderWidth: 1
+    },
+    bottomButtons: {
+        height: '80%',
+        width: '35%',
+        elevation: 4,
+        borderRadius: 5,
+        justifyContent: 'center',
+        textAlign: 'center',
+        alignItems: 'center'
+
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 24
+    },
+    bottomScreen: {
+        justifyContent: 'space-around',
+        alignItems: 'center',
+
+        flexDirection: 'row',
+        height: '100%'
+    },
+    nameText: {
+        textAlign: 'center',
+        color: '#1170ba',
+        fontSize: 20
     }
 })
 
